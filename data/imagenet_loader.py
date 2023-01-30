36b5e1bfb09e62eb84ec10525df9e2e5c8111a66
Aleksandr Ershov <aershov@wks-22-0368>
2023-01-30 16:02:40 +0100

    Update imagenet_loader.py and config.yaml to get dataset of different imagenet classes

--- data/imagenet_loader.py
+++ data/imagenet_loader.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
@@ -18,29 +20,6 @@ def get_dataset_config(cfg):
     return dataset_config
 
 def load_custom(cfg, dataset_config, device):
-    dataset_inst = lambda *fn_args, **fn_kwargs: datasets.CustomDataset(
-        cfg.dataset, *fn_args, **fn_kwargs, root_dir=cfg.data_path)
-
-    img_size = dataset_config.resolution
-    img_size_train = img_size * 2 if dataset_config.augment_p > 0 else img_size
-    dataset = dataset_inst('train',
-                           img_size=img_size_train,
-                           crop=False,
-                           add_mirrored=True)
-    dataset_fid = dataset_inst('train',
-                               img_size=img_size,
-                               crop=True,
-                               add_mirrored=False)
-    loader = torch.utils.data.DataLoader(dataset,
-                                         shuffle=False,
-                                         batch_size=32,
-                                         num_workers=8,
-                                         pin_memory=False)
-    loader_fid = torch.utils.data.DataLoader(dataset_fid,
-                                             shuffle=False,
-                                             batch_size=32,
-                                             num_workers=8,
-                                             pin_memory=False)
 
     train_split = DatasetSplit(device)
     train_eval_split = DatasetSplit(device)
@@ -52,32 +31,62 @@ def load_custom(cfg, dataset_config, device):
     all_poses = []
     all_focal = []
     all_bbox = []
+    all_classes_id = []
     all_classes = []
 
     all_poses_fid = []
     all_focal_fid = []
     all_bbox_fid = []
+    all_classes_id_fid = []
     all_classes_fid = []
 
-    for _, sample in enumerate(tqdm(loader)):
-        if dataset_config.augment_p > 0:
-            all_images_highres.append(sample['img'].clamp(-1, 1).permute(
-                0, 2, 3, 1))
-            all_images.append(
-                F.avg_pool2d(sample['img'], 2).clamp(-1, 1).permute(0, 2, 3, 1))
-        else:
-            all_images.append(sample['img'].clamp(-1, 1).permute(0, 2, 3, 1))
-        all_poses.append(sample['pose'])
-        all_focal.append(sample['focal'])
-        all_bbox.append(sample['normalized_bbox'])
-        all_classes.append(sample['class'])
+    for imagenet_cls in cfg.dataset:
+        dataset_inst = lambda *fn_args, **fn_kwargs: datasets.CustomDataset(
+            imagenet_cls, *fn_args, **fn_kwargs, root_dir=cfg.data_path
+        )
 
-    for _, sample in enumerate(tqdm(loader_fid)):
-        all_images_fid.append(sample['img'].clamp(-1, 1).permute(0, 2, 3, 1))
-        all_poses_fid.append(sample['pose'])
-        all_focal_fid.append(sample['focal'])
-        all_bbox_fid.append(sample['normalized_bbox'])
-        all_classes_fid.append(sample['class'])
+        img_size = dataset_config.resolution
+        img_size_train = img_size * 2 if dataset_config.augment_p > 0 else img_size
+        dataset = dataset_inst('train',
+                            img_size=img_size_train,
+                            crop=False,
+                            add_mirrored=True)
+        dataset_fid = dataset_inst('train',
+                                img_size=img_size,
+                                crop=True,
+                                add_mirrored=False)
+        loader = torch.utils.data.DataLoader(dataset,
+                                            shuffle=False,
+                                            batch_size=32,
+                                            num_workers=8,
+                                            pin_memory=False)
+        loader_fid = torch.utils.data.DataLoader(dataset_fid,
+                                                shuffle=False,
+                                                batch_size=32,
+                                                num_workers=8,
+                                                pin_memory=False)
+
+        for _, sample in enumerate(tqdm(loader)):
+            if dataset_config.augment_p > 0:
+                all_images_highres.append(sample['img'].clamp(-1, 1).permute(
+                    0, 2, 3, 1))
+                all_images.append(
+                    F.avg_pool2d(sample['img'], 2).clamp(-1, 1).permute(0, 2, 3, 1))
+            else:
+                all_images.append(sample['img'].clamp(-1, 1).permute(0, 2, 3, 1))
+            all_poses.append(sample['pose'])
+            all_focal.append(sample['focal'])
+            all_bbox.append(sample['normalized_bbox'])
+            all_classes_id.append(sample['class_id'])
+            all_classes.append(sample['class'])
+
+        for _, sample in enumerate(tqdm(loader_fid)):
+            all_images_fid.append(sample['img'].clamp(-1, 1).permute(0, 2, 3, 1))
+            all_poses_fid.append(sample['pose'])
+            all_focal_fid.append(sample['focal'])
+            all_bbox_fid.append(sample['normalized_bbox'])
+            all_classes_id_fid.append(sample['class_id'])
+            all_classes_fid.append(sample['class'])
 
     train_split.images = torch.cat(all_images, dim=0)
     train_eval_split.images = torch.cat(all_images_fid, dim=0)
@@ -89,21 +98,22 @@ def load_custom(cfg, dataset_config, device):
     train_split.tform_cam2world = torch.cat(all_poses, dim=0)
     train_split.focal_length = torch.cat(all_focal, dim=0).squeeze(1)
     train_split.bbox = torch.cat(all_bbox, dim=0)
-    train_split.classes = torch.cat(all_classes, dim=0)
-    train_split.num_classes = train_split.classes.max().item() + 1
+    train_split.classes_id = torch.cat(all_classes_id, dim=0)
+    train_split.classes = np.concatenate(all_classes, axis=0)
+    train_split.num_classes = train_split.classes_id.max().item() + 1
+    for id_, class_ in zip(train_split.classes_id, train_split.classes):
+        if class_ not in train_split.class_to_id:
+            train_split.class_to_id[class_] = id_
+            train_split.id_to_class[id_] = class_
 
     train_eval_split.tform_cam2world = torch.cat(all_poses_fid, dim=0)
     train_eval_split.focal_length = torch.cat(all_focal_fid, dim=0).squeeze(1)
     train_eval_split.bbox = torch.cat(all_bbox_fid, dim=0)
-    train_eval_split.classes = torch.cat(all_classes_fid, dim=0)
+    train_eval_split.classes_id = torch.cat(all_classes_id_fid, dim=0)
+    train_eval_split.classes = np.concatenate(all_classes_fid, axis=0)
     train_eval_split.num_classes = train_split.num_classes
-
-    train_split.bbox = None
-
-    train_split.classes = None
-    train_split.num_classes = None
-    train_eval_split.classes = None
-    train_eval_split.num_classes = None
+    train_eval_split.class_to_id = train_split.class_to_id
+    train_eval_split.id_to_class = train_eval_split.id_to_class
 
     return train_split, train_eval_split, test_split
 
@@ -141,8 +151,11 @@ class DatasetSplit:
         self.focal_length = None
         self.bbox = None
         self.center = None
+        self.classes_id = None
         self.classes = None
         self.num_classes = None
+        self.class_to_id = {}
+        self.id_to_class = {}
 
         self.fid_stats = None
         self.eval_indices = None
